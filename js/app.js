9aace1e71fa617f41720b012f553dca84c763f4b
meshnesh <tonnie.nesh@gmail.com>
2016-09-15 14:49:53 +0300

    changed the sites design

---
+++ js/app.js
@@ -0,0 +1,24 @@
+// ********************************************************
+// NAV BAR
+// 
+$(function () {
+    // Returns width of browser viewport
+    var browser = $(window).width();
+    // Returns width of HTML document
+    var document = $(document).width();
+    $('.nav .toggle-nav').click(function (e) {
+        $('.nav .nav-mobile').addClass('style-mobile').slideToggle('slow');
+        e.preventDefault();
+    });
+});
+
+
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 50) {
+        $("nav").addClass("scrolling");
+    }
+    else {
+        $("nav").removeClass("scrolling");
+    }
+});
